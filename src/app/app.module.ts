290a6d7d3913243b4bb5b06d0a9908456d8f8bbe
Sathish-509 <sathish9yerramsetti@gmail.com>
2020-02-16 22:10:52 +0800

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { OutputGraphComponent } from './output-graph/output-graph.component';
 
 @NgModule({
   imports: [
@@ -33,6 +34,7 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductDetailsComponent,
     CartComponent,
     ShippingComponent,
+    OutputGraphComponent,
   ],
   bootstrap: [ AppComponent ],
   providers: [CartService]
